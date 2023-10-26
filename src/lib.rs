7f05a5ceea29f0917385ff3f72bc875823ee442c
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-25 18:05:48 -0600

    implement new & next_token methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq, Eq)]
 enum TokenType {
     ILLEGAL,
     EOF,
@@ -21,9 +22,60 @@ enum TokenType {
     RBRACE,
 }
 
+#[derive(PartialEq, Eq)]
 struct Token {
     ttype: TokenType,
-    literal: String,
+    literal: Option<char>,
+}
+
+struct Lexer {
+    input: Vec<char>,
+    position: usize,
+    read_position: usize,
+    ch: Option<char>,
+}
+
+impl Lexer {
+    fn new(input: String) -> Self {
+        let input_chars = input.chars().collect();
+        let mut l = Lexer {
+            input: input_chars,
+            position: 0,
+            read_position: 0,
+            ch: None,
+        };
+        
+        l.read_char();
+        l
+    }
+
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len().try_into().unwrap() {
+            self.ch = None;
+        } else {
+            self.ch = Some(self.input[self.read_position]);
+        }
+
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+
+    fn next_token(&mut self) -> Token {
+        let token = match self.ch {
+            Some('=') => Token { ttype: TokenType::ASSIGN, literal: self.ch },
+            Some(';') => Token { ttype: TokenType::SEMICOLON, literal: self.ch },
+            Some('(') => Token { ttype: TokenType::LPAREN, literal: self.ch },
+            Some(')') => Token { ttype: TokenType::RPAREN, literal: self.ch },
+            Some(',') => Token { ttype: TokenType::COMMA, literal: self.ch },
+            Some('+') => Token { ttype: TokenType::PLUS, literal: self.ch },
+            Some('{') => Token { ttype: TokenType::LBRACE, literal: self.ch },
+            Some('}') => Token { ttype: TokenType::RBRACE, literal: self.ch },
+            _ => Token { ttype: TokenType::EOF, literal: None},
+        };
+
+        self.read_char();
+        token
+    }
 }
 
 #[cfg(test)]
@@ -34,20 +86,22 @@ mod test {
     fn next_token() {
         let input = String::from("=+(){},;");
         let tokens = vec![
-            Token { ttype: TokenType::ASSIGN, literal: String::from("=") },
-            Token { ttype: TokenType::PLUS, literal: String::from("+") },
-            Token { ttype: TokenType::LPAREN, literal: String::from("(") },
-            Token { ttype: TokenType::RPAREN, literal: String::from(")") },
-            Token { ttype: TokenType::LBRACE, literal: String::from("{") },
-            Token { ttype: TokenType::RBRACE, literal: String::from("}") },
-            Token { ttype: TokenType::COMMA, literal: String::from(",") },
-            Token { ttype: TokenType::SEMICOLON, literal: String::from(";") },
-            Token { ttype: TokenType::EOF, literal: String::from("") },
+            Token { ttype: TokenType::ASSIGN, literal: Some('=') },
+            Token { ttype: TokenType::PLUS, literal: Some('+') },
+            Token { ttype: TokenType::LPAREN, literal: Some('(') },
+            Token { ttype: TokenType::RPAREN, literal: Some(')') },
+            Token { ttype: TokenType::LBRACE, literal: Some('{') },
+            Token { ttype: TokenType::RBRACE, literal: Some('}') },
+            Token { ttype: TokenType::COMMA, literal: Some(',') },
+            Token { ttype: TokenType::SEMICOLON, literal: Some(';') },
+            Token { ttype: TokenType::EOF, literal: None },
         ];
 
-        let l = Token::new(input);
+        let mut l = Lexer::new(input);
+
         for token in tokens {
             let tok = l.next_token();
+            println!("{:?}", tok.literal);
             assert_eq!(tok.ttype, token.ttype);
             assert_eq!(tok.literal, token.literal);
         }
