974061856b4d1d3927987401f2b78724a62abeba
J. Jaime Paredes <jjpp109309@gmail.com>
2023-11-13 20:35:51 -0600

    change repl to module

---
+++ src/repl/mod.rs
@@ -0,0 +1,32 @@
+use std::io::{stdin, stdout, Write};
+use crate::{Lexer, TokenType};
+
+pub fn start() {
+    println!("Hello! this is the Monkney programming language!");
+    println!("Feel free to type in commands\n");
+
+    let prefix = String::from("|monkey> ");
+    let mut input = String::new();
+
+    loop {
+        print!("{}", prefix);
+        let _ = stdout().flush();
+        let _ = stdin().read_line(&mut input);
+
+        if input == String::from("exit\n") {
+            break
+        }
+
+        let mut l = Lexer::new(&input);
+        loop {
+            let tok = l.next_token();
+            match tok.ttype {
+                TokenType::Eof => break,
+                _ => println!("{:?}", tok)
+            }
+        }
+
+        input.clear();
+    }
+    println!("Bye!");
+}
